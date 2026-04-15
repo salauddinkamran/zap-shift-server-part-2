7d57b9d8cd49997faa6adf2c25b8ef5abdddd989
salauddin kamran <kamransalauddin186@gmail.com>
2026-04-15 22:16:29 +0600

    added user get api or some apis in this server

--- index.js
+++ index.js
@@ -68,6 +68,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
 
     // users related apis
+
+    app.get("/users", verifyFBToken, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       user.role = "user";
@@ -285,9 +291,16 @@ async function run() {
         };
         const updateResult = await usersCollection.updateOne(
           userQuery,
-          updateUser
+          updateUser,
         );
       }
+      res.send(result);
+    });
+
+    app.delete("/riders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ridersCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -295,7 +308,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
     // Ensures that the client will close when you finish/error
